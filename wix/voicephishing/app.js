01971905b2c23f7bd75521f77361af71bc9ed8d3
jeongwon6955 <choirio2467@gmail.com>
2025-06-13 19:51:31 +0900

    .

--- wix/voicephishing/app.js
+++ wix/voicephishing/app.js
@@ -19,3 +19,25 @@ topbtn.addEventListener('click', function() {
         block: 'start'
     })
 })
+
+//버튼 슬라이드
+;
+const slide = document.querySelector('.cases');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let currenttarget = 0;
+
+function slideshow(){
+    slide.style.transform = `translateX(-${currenttarget * 450}px)`
+};
+
+next.addEventListener('click', function() {
+    currenttarget++
+    slideshow();
+});
+
+prev.addEventListener('click', function() {
+    currenttarget--
+    slideshow();
+});
