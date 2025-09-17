d75d74ead6084d32a0cb31c561294ab4899ec7c3
jeongwon6955 <choirio2467@gmail.com>
2025-09-17 20:54:30 +0900

    .

--- skills baseball park/B-Module/goods/done/script.js
+++ skills baseball park/B-Module/goods/done/script.js
@@ -7,3 +7,28 @@ fetch("../json/goods.json")
     jsonData = data;
     loadWindow(jsonData);
 });
+
+// 판매량 가격의 오름차순 내림차순
+
+const SAsc = document.querySelector(".s_asc");
+const SDesc = document.querySelector(".s_desc");
+const PAsc = document.querySelector(".p_asc");
+const PDesc = document.querySelector(".p_desc");
+const imgBox = document.querySelectorAll(".goo_img");
+
+let saleArrey = [];
+
+function loadWindow(jsonData) {
+    saleArrey = jsonData.data.map(sales => sales);
+    SAsc.addEventListener("click", function() {
+        saleArrey.sort((a,b) => b.sale - a.sale);
+        imgBox.forEach((box, index) => {
+            const item = saleArrey[index];
+           if (item) {
+            const img = document.createElement('img');
+            img.src = item.img;
+            box.appendChild(img);
+            }
+        });
+    });
+};
